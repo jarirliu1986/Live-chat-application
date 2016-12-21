6db440f8d4324b60315a53f6862c454ed52ec361
jarirliu1986 <289374247@qq.com>
2016-12-20 17:15:43 -0800

    add message to browser UI

--- public/js/app.js
+++ public/js/app.js
@@ -7,4 +7,31 @@ socket.on('connect', function () {
 socket.on('message', function (message) {
 	console.log('new message: ');
 	console.log(message.text);
+
+	$('.messages').append('<p>' + message.text + '</p>');
+	
 });
+
+//handle the message submitted
+
+var $form = $('#message-form');
+console.log($form);
+
+$form.on('submit', function (event) {
+	event.preventDefault();
+
+	var $message = $form.find('input[name=message]');
+
+	socket.emit('message', {
+		text: $message.val()
+	});
+
+	$message.val('');
+});
+
+function send() {
+	socket.emit('message', {
+		text: document.getElementById('sendmessage').value
+	});
+	document.getElementById('sendmessage').value = '';
+}
